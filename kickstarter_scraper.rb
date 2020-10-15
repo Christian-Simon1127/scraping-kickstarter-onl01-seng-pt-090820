34f3568a13cf535f4f01c4fbb975e3137c2c83ba
Christian Simon <csimon1127@gmail.com>
2020-10-15 00:55:52 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -6,7 +6,7 @@ require "pry"
   #title: project.css("h2.bbcard_name strong a").text
   #image link: project.css("div.project-thumbnail a img").attribute(src).value
   #description: project.css("p.bbcard_blurb").text
-  #location: project.css("project-meta li a").text
+  #location: project.css(".project-meta li a").text
   
 def create_project_hash
   # write your code here
