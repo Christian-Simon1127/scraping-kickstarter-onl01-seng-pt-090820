c3597bd4b2d072f8f684cae0c888887fc4933e15
Christian Simon <csimon1127@gmail.com>
2020-10-15 01:39:33 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -18,7 +18,12 @@ def create_project_hash
   #iterating through the projects 
   kickstarter.css("li.project.grid_4").each { |project|
     title = project.css("h2.bbcard_name strong a").text
-    projects[title.to_sym] = {}
+    projects[title.to_sym] = {
+      :image_link => project.css("div.project-thumbnail a img").attribute(src).value,
+      :description => project.css("p.bbcard_blurb").text,
+      :location = 
+      
+    }
   }
 
   #binding.pry
